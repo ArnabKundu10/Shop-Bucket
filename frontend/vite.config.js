7fd48dcfb4614daf85dce67666a2d3523530770f
Arnab <arnabk2001@gmail.com>
2024-10-08 17:29:40 +0530

    Change before deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["antd"], // Add antd to the external list if needed
-    },
-  },
   plugins: [react()],
 });
